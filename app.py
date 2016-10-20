7cfe43aee8fdf91737228c0a65f590ea1d9010bf
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-10-20 14:56:54 +0200

    Can add a customer with API call

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ import json
 from Client import *
 app = Flask(__name__)
 
+# Dictionnary contains every customers added with createCustomer method
+customers = []
+
 @app.route("/")
 def index():
     return 'Hello World !'
+
+@app.route('/create', methods=['POST'])
+def createCustomer():
+    # Send data in POST method
+    if request.method == 'POST':
+        customers.append(request.form)
+    return json.dumps(customers)
 
 if __name__ == "__main__":
     app.run('192.168.33.22', debug=True)
